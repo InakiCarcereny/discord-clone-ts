b38b6c96c047635db859598984aead0910aba654
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-03 20:26:10 -0300

    feat: add an optional prop to change the size of the global button component

--- client/src/app/components/button/Button.tsx
+++ client/src/app/components/button/Button.tsx
@@ -1,14 +1,17 @@
 export interface ButtonProps {
   label: string;
   type?: ButtonType;
+  size?: string;
 }
 
 export type ButtonType = "submit" | "button";
 
-export function Button({ label, type }: ButtonProps) {
+export function Button({ label, type, size }: ButtonProps) {
   return (
     <button
-      className="text-white bg-[#5865f2] hover:bg-blue-700 duration-200 font-bold py-2 px-4 rounded h-12 w-full"
+      className={` text-white bg-[#5865f2] hover:bg-blue-700 duration-200 font-bold py-2 px-4 rounded h-12 ${
+        size ? size : "w-full"
+      }`}
       type={type}
     >
       {label}
