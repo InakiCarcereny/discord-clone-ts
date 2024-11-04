29e76c21a7ef9e66a119366c3917f548225c89bd
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-04 10:53:40 -0300

    add: create user buttons and export like constant

---
+++ client/src/app/consts/userButtons.ts
@@ -0,0 +1,18 @@
+import { Headphones } from "../icons/Headphones";
+import { Microphone } from "../icons/Microphone";
+import { Settings } from "../icons/Settings";
+
+export const userButtons = [
+  {
+    id: 1,
+    icon: Microphone({ className: "w-5 h-5" }),
+  },
+  {
+    id: 2,
+    icon: Headphones({ className: "w-6 h-6" }),
+  },
+  {
+    id: 3,
+    icon: Settings({ className: "w-5 h-5" }),
+  },
+];
