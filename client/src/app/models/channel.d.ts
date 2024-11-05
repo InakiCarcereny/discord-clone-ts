54719bc821618213f70d9c8ca669520235a860f6
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-05 15:40:29 -0300

    feat: add TypeScript interfaces for Channel and Event models with necessary properties and types defined

---
+++ client/src/app/models/channel.d.ts
@@ -0,0 +1,9 @@
+export interface Channel {
+  _id: string;
+  name: string;
+  server: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export type Id = string;
---
+++ client/src/app/models/event.d.ts
@@ -0,0 +1,15 @@
+export interface Event {
+  _id: string;
+  ubication: string;
+  theme: string;
+  dateInit: string;
+  dateEnd: string;
+  timeInit: string;
+  timeEnd: string;
+  frequency: string;
+  description: string;
+  logo: string;
+  server: string;
+}
+
+export type Id = string;
