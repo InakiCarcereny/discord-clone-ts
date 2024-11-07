0c6bacaf01a62263012b402b519fe8cf225bc488
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-07 13:48:03 -0300

    feat: add ArrowRight icon component for improved SVG usage in the application

---
+++ client/src/app/icons/ArrowRight.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import type { SVGProps } from "react";
+
+export function ArrowRight(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m10 17l5-5l-5-5"
+      ></path>
+    </svg>
+  );
+}
