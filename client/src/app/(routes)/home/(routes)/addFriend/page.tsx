3f3b3479032792c8c18d860eb8e3d487a5dc53e5
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-12 19:28:33 -0300

    feat: implement form for adding friends with username input and handle submission using React Hook Form for better UX

--- client/src/app/(routes)/home/(routes)/addFriend/page.tsx
+++ client/src/app/(routes)/home/(routes)/addFriend/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+
 import { Button } from "@/app/components";
 
+import { useForm } from "react-hook-form";
+import { useFriendRequest } from "../../context/friendRequest";
+import { useAuth } from "@/app/context/auth";
+
 export default function AddFriend() {
+  const { addFriend } = useFriendRequest();
+
+  const { user } = useAuth();
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit(async (data) => {
+    addFriend(user?.username, data.recipient);
+  });
+
   return (
     <div className="flex flex-col px-6">
       <header className="flex flex-col gap-2 mt-4">
@@ -10,14 +26,18 @@ export default function AddFriend() {
         </p>
       </header>
 
-      <div className="flex items-center justify-between bg-zinc-900 text-gray-300 px-4 py-2 rounded-[4px] w-full mt-6">
+      <form
+        onSubmit={onSubmit}
+        className="flex items-center justify-between bg-zinc-900 text-gray-300 px-4 py-2 rounded-[4px] w-full mt-6"
+      >
         <input
           type="text"
           className="w-full bg-transparent focus:outline-none"
           placeholder="Username"
+          {...register("recipient")}
         />
         <Button label="Add friend" type="submit" size="w-[150px]" />
-      </div>
+      </form>
     </div>
   );
 }
