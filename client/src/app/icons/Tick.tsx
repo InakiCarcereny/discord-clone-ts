b13b9697a7a12d38d8388e0216ceed588883388e
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-12 19:28:06 -0300

    feat: add Tick icon component as an SVG for use across the application

---
+++ client/src/app/icons/Tick.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { SVGProps } from "react";
+
+export function Tick(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M21 7L9 19l-5.5-5.5l1.41-1.41L9 16.17L19.59 5.59z"
+      ></path>
+    </svg>
+  );
+}
