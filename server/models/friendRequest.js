688054bcdb299c669c4900fd8982ca616947ddb7
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-14 23:53:47 -0300

    style: reformat friend request and friend schemas for improved readability and consistency in the models files

--- server/models/friendRequest.js
+++ server/models/friendRequest.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const friendRequestSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   recipient: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
--- server/models/friends.js
+++ server/models/friends.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 const friendSchema = new mongoose.Schema({
-  user1: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  user2: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  user1: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  user2: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
