bf79e9334a8f89c9ee866ee3e1a21c95fb80ab19
inakicarcereny <inakicarce@hotmail.com.ar>
2024-11-15 16:34:49 -0300

    feat: add success message display and styling adjustments in AddFriend component to improve user feedback and experience

--- client/src/app/(routes)/home/(routes)/addFriend/page.tsx
+++ client/src/app/(routes)/home/(routes)/addFriend/page.tsx
@@ -5,8 +5,11 @@ import { Button } from "@/app/components";
 import { useForm } from "react-hook-form";
 import { useFriendRequest } from "../../context/friendRequest";
 import { useAuth } from "@/app/context/auth";
+import { useState } from "react";
 
 export default function AddFriend() {
+  const [message, setMessage] = useState("");
+
   const { addFriend } = useFriendRequest();
 
   const { user } = useAuth();
@@ -15,10 +18,11 @@ export default function AddFriend() {
 
   const onSubmit = handleSubmit(async (data) => {
     addFriend(user?.username, data.recipient);
+    setMessage("Friend added successfully");
   });
 
   return (
-    <div className="flex flex-col px-6">
+    <div className="flex flex-col px-6 gap-2">
       <header className="flex flex-col gap-2 mt-4">
         <h1 className="text-white font-semibold">ADD FRIEND</h1>
         <p className="text-xs text-zinc-300 font-semibold">
@@ -28,7 +32,7 @@ export default function AddFriend() {
 
       <form
         onSubmit={onSubmit}
-        className="flex items-center justify-between bg-zinc-900 text-gray-300 px-4 py-2 rounded-[4px] w-full mt-6"
+        className="flex items-center justify-between bg-zinc-900 text-gray-300 px-4 py-2 rounded-[4px] w-full mt-6 border border-green-400"
       >
         <input
           type="text"
@@ -38,6 +42,8 @@ export default function AddFriend() {
         />
         <Button label="Add friend" type="submit" size="w-[150px]" />
       </form>
+
+      <span className="text-green-400 text-sm font-semibold">{message}</span>
     </div>
   );
 }
